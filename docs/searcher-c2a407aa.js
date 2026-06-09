a480e627d4fc536cb9f1f407687e07713bd87052
sm <santimir@pm.me>
2026-06-09 21:05:29 +0100

    compile

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4c2b778f.js',
+                path_to_root + 'searchindex-b611ad2a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
